dc4adab89c8f5d6e3cf14da916746322c0817a69
victor gabriel <vgvf@gmail.com>
2023-06-03 14:10:53 -0300

    adicionei o cino

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import react from 'react';
 import * as S from './styles';
 
 import logo from '../../assets/logo.png';
+import bell from '../../assets/logo.png';
 
 function Header() {
     return (
@@ -13,6 +14,10 @@ function Header() {
             <a href="#">INÍCIO</a>
             <a href="#">NOVA TAREFA</a>
             <a href="#">SINCRONIZAR CELULAR</a>
+            <a href="#" id="notification" >
+              <img src={bell} alt="notificação" />
+              <span>5</span>
+            </a>
           </S.RightSide>
         </S.Container>
     )
